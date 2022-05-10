8036e4c0bffa95be9c8f28e19559c3283251ad31
damanbhatia462000 <73290005+damanbhatia462000@users.noreply.github.com>
2022-05-10 11:50:33 +0530

    Delete inventory-management-system directory

--- inventory-management-system/src/main/java/com/wipro/velocity/ims/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wipro.velocity.ims.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wipro.velocity.ims.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
